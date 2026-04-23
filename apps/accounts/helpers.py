0e5816f6829e42bdcd3bb1399b6c028e96d4ad98
Belam Muia <belammuia0@gmail.com>
2026-04-23 21:37:52 +0300

    add google auth

--- apps/accounts/helpers.py
+++ apps/accounts/helpers.py
@@ -1,7 +1,10 @@
 import secrets
 from django.core.cache import cache
 from .tasks import send_otp_email
-
+import requests
+from django.conf import settings
+from urllib.parse import urlencode
+from django.shortcuts import redirect
 
 def generate_email_otp(email: str) -> str:
     email = email.strip().lower()
@@ -24,3 +27,56 @@ def verify_email_otp(email: str, user_otp: str) -> bool:
         cache.delete(f"otp:{email}")
         return True
     return False
+
+
+def google_login():
+    params = {
+        "client_id": settings.CLIENT_GOOGLE_ID,
+        "redirect_uri": settings.CLIENT_GOOGLE_REDIRECT,
+        "response_type": "code",
+        "scope": "openid email profile",
+        "access_type": "offline",
+        "prompt": "consent",
+    }
+
+    url = "https://accounts.google.com/o/oauth2/v2/auth?" + urlencode(params)
+
+    return redirect(url)
+
+
+
+def get_google_token(code: str):
+    url = "https://oauth2.googleapis.com/token"
+
+    data = {
+        "code": code,
+        "client_id": settings.CLIENT_GOOGLE_ID,
+        "client_secret": settings.CLIENT_GOOGLE_SECRET,
+        "redirect_uri": settings.CLIENT_GOOGLE_REDIRECT,
+        "grant_type": "authorization_code",
+    }
+
+    response = requests.post(url, data=data)
+
+    response_data = response.json()
+
+    if "access_token" not in response_data:
+        raise Exception(f"Failed to get token: {response_data}")
+
+    return response_data["access_token"]
+
+
+def get_google_user_info(access_token: str):
+    url = "https://www.googleapis.com/oauth2/v2/userinfo"
+
+    headers = {  
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+
+    if "email" not in response_data:
+        raise Exception(f"Failed to fetch user's info: {response_data}")
+
+    return response_data
