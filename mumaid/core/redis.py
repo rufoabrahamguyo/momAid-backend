a067b2a0606cf0f45f9b97fd64f8be9d98a4d270
Belam Muia <belammuia0@gmail.com>
2026-04-24 21:15:11 +0300

    fix redis

--- mumaid/core/redis.py
+++ mumaid/core/redis.py
@@ -3,4 +3,8 @@ import redis
 
 
 def redis_client():
-    return redis.from_url(os.getenv("REDIS_URL"))
+    return redis.from_url(
+        os.getenv("REDIS_URL"),
+        decode_responses=True,
+        ssl_cert_reqs="required"  
+    )
