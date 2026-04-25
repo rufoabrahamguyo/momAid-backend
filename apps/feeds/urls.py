de3d77381040a4bc412b598a725ff926583b364d
Belam Muia <belammuia0@gmail.com>
2026-04-25 21:47:01 +0300

    fix: rename url_patterns to urlpatterns

--- apps/feeds/urls.py
+++ apps/feeds/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import UploadUserVideoView, GetAllVideosView, UserSpecificVideoView
 
-url_patterns = [
+urlpatterns = [
     path("/v1/upload/video/", UploadUserVideoView.as_view(), name='user-upload-video'),
     path("/v1/user/specific/videos", UserSpecificVideoView.as_view(), name="user-specific-video"),
     path("/v1/videos/all/", GetAllVideosView.as_view(), name="get-all-videos")
