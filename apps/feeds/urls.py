9ce8d5295cc58bc68b82c9f74cbacc84944ecc99
Belam Muia <belammuia0@gmail.com>
2026-04-25 21:57:55 +0300

    remove trailing slash on urls

--- apps/feeds/urls.py
+++ apps/feeds/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import UploadUserVideoView, GetAllVideosView, UserSpecificVideoView
 
 urlpatterns = [
-    path("/v1/upload/video/", UploadUserVideoView.as_view(), name='user-upload-video'),
-    path("/v1/user/specific/videos", UserSpecificVideoView.as_view(), name="user-specific-video"),
-    path("/v1/videos/all/", GetAllVideosView.as_view(), name="get-all-videos")
+    path("v1/upload/video/", UploadUserVideoView.as_view(), name='user-upload-video'),
+    path("v1/user/specific/videos", UserSpecificVideoView.as_view(), name="user-specific-video"),
+    path("v1/videos/all/", GetAllVideosView.as_view(), name="get-all-videos")
 ]
