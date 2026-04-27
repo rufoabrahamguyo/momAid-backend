ddb474eb798b2eece7e5a8b3570daaeee9f1f2e2
Belam Muia <belammuia0@gmail.com>
2026-04-27 16:37:39 +0300

    remove seed data

--- start.sh
+++ start.sh
@@ -6,8 +6,6 @@ echo "Starting Django..."
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
-echo "seed data"
-python manage.py seed_stress_data
 
 
 gunicorn mumaid.wsgi:application --bind 0.0.0.0:8000 &
