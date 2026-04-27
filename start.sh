a392c065600de82555fa4b8cb99253b2fe5217fa
Belam Muia <belammuia0@gmail.com>
2026-04-27 11:16:33 +0300

    add seed command

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ echo "Starting Django..."
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
+echo "seed data"
+python manage.py seed_stress_data
+
 
 gunicorn mumaid.wsgi:application --bind 0.0.0.0:8000 &
 
