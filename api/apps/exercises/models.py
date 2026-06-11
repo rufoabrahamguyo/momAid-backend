3b0b22b59238e67ddd59c47ac6b2cde5d1726d76
belamx <belammuia0@gmail.com>
2026-06-11 11:40:49 +0300

    fix(models): remove extra characters and the db model importation

--- api/apps/exercises/models.py
+++ api/apps/exercises/models.py
@@ -1,15 +1,15 @@
+from django.db import models
 from core.models import TimeStampedModel
-b;
+
+
 class Exercise(TimeStampedModel):
     title = models.CharField(max_length=100)
     description = models.TextField()
     video_file = models.URLField()
     thumbnail_file = models.URLField(blank=True)
 
-
     class Meta:
         ordering = ["-created_at"]
-
 
     def __str__(self) -> str:
         return self.title
